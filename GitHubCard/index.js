06a6785fbc7b1407385a81050d625d2220e269bc
jcpcabanada <jcpcabanada@gmail.com>
2021-10-28 15:35:58 -0700

    test push
    Took 2 minutes

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -27,7 +27,7 @@ axios
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+//test
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
